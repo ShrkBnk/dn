8180bacb5b5f32230c2d905f8618b2b30582ab8c
Joonas <joonas@crabcode.ee>
2021-07-22 00:45:14 +0300

    Update city update value

--- src/server/services/db/insertCityWeather.ts
+++ src/server/services/db/insertCityWeather.ts
@@ -4,7 +4,7 @@ import { handleSpaces } from "../../../helpers";
 
 export const insertCityWeather = async (city: string) => {
   let cityWithoutSpaces = handleSpaces(city);
-  const weatherFullData = getWeatherData(cityWithoutSpaces);
+  const weatherFullData = getWeatherData(city);
   let currentWeather: any = {};
   await weatherFullData.then((res: any) => (currentWeather = res));
   const sql = `INSERT INTO ${cityWithoutSpaces} (address, datetime, temp, humidity, wspd, pressure) VALUES('${currentWeather.address}', to_timestamp(${currentWeather.datetime}), ${currentWeather.temp}, ${currentWeather.humidity}, ${currentWeather.wspd}, ${currentWeather.pressure})`;
